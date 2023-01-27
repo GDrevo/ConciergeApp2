de1cb8994ef9108f6d2d82f0a4494ef0e97cba99
guillaume drevo <guillaume.drevo@gmail.com>
2023-01-27 18:49:32 +0100

    added simple_calendar gem and started implementing it

--- app/controllers/availabilities_controller.rb
+++ app/controllers/availabilities_controller.rb
@@ -2,6 +2,8 @@ class AvailabilitiesController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
+    start_date = params.fetch(:start_date, Date.today).to_date
 
+    @availabilities = Availability.where(start_time: start_date.beginning_of_week..start_date.end_of_week)
   end
 end
